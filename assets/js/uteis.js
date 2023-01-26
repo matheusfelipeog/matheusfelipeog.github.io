8836a8037f73210fae16dd2be4f1e818ed627f3b
Matheus Felipe <matheusfelipeog@protonmail.com>
2023-01-26 03:56:15 -0300

    Delete old page and assets

--- assets/js/uteis.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function main(){
-
-    // Bloquear drag das imagens para não prejudicar a experiência
-    document.querySelectorAll('.logo').forEach(logo => {
-        logo.setAttribute('ondragstart', 'return false')
-    });
-
-});
